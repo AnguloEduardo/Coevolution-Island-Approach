5ed4e1d8ac586e5f07ac2419ca7418a3eabe43f6
Eduardo Angulo <a00820188@tec.mx>
2022-08-12 13:53:27 -0500

    HyperHeuristic Code

--- DirectSolver/formato.py
+++ DirectSolver/formato.py
@@ -1,7 +1,7 @@
 file_name = ['ks_4_0', 'ks_19_0', 'ks_30_0', 'ks_40_0', 'ks_45_0', 'ks_50_0', 'ks_50_1',
              'ks_60_0', 'ks_82_0', 'ks_100_0', 'ks_100_1', 'ks_100_2', 'ks_106_0', 'ks_200_0', 'ks_200_1',
              'ks_300_0', 'ks_400_0', 'ks_500_0', 'ks_1000_0', 'ks_10000_0']
-format_data = open('experiments\\data.txt', 'a')
+format_data = open('DirectSolver/experiments\\data.txt', 'a')
 number_islands = 2
 
 def formatting():
--- DirectSolver/graphs.py
+++ DirectSolver/graphs.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-formatted_data = open('experiments\\data.txt', 'r')
+formatted_data = open('DirectSolver/experiments\\data.txt', 'r')
 temp = []
 file_name = ['ks_4_0', 'ks_19_0', 'ks_30_0', 'ks_40_0', 'ks_45_0', 'ks_50_0', 'ks_50_1',
              'ks_60_0', 'ks_82_0', 'ks_100_0', 'ks_100_1', 'ks_100_2', 'ks_106_0', 'ks_200_0', 'ks_200_1',
